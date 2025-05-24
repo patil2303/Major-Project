6eb62b051561ab78c9bf764ba79db1c72568c204
Shreyas Patil <shreyasspatil23@gmail.com>
2025-05-24 17:01:52 +0530

    Add Projrct Files

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const Mongo_Url = "mongodb://127.0.0.1:27017/wonderlust"
+
+main()
+.then(() => {
+    console.log("connected to database");
+})
+.catch((err) => {
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(Mongo_Url);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "68028a5d369425c9195e1d0c" }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+}
+
+initDB();
